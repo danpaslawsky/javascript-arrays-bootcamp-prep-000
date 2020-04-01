14b6bcdb94f34edb49828a134da3e2a3225eebed
danpaslawsky <danpaslawsky@gmail.com>
2020-04-01 18:10:41 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  //var newArray = array;
+  return [element,...array];
+}
+
+function addElementToEndOfArray(array, element){
+ return [...array,element];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}   
+
+   
+   
+   
+   
