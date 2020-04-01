a8721e873f1586b54895f0a579c4b83c639ae715
danpaslawsky <danpaslawsky@gmail.com>
2020-04-01 18:46:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,8 @@ function destructivelyAddElementToEndOfArray(array, element){
 }   
 
 function accessElementInArray(array, index){
-  var numbers = ;
+  var numbers = accessElementInArray;
+  numbers[2];
 }
    
    
