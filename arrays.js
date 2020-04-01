2883e5a4173d2c93886965d2140468066e3951c2
danpaslawsky <danpaslawsky@gmail.com>
2020-04-01 20:00:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,7 +33,7 @@ function accessElementInArray(array, index){
  }
  
 function destructivelyRemoveElementFromEndOfArray(array){
-  return array.slice(0, array.length);
+  return array.slice(0, array.length -1);
 }  
  
    
